a8e64998f9ede58d0eb9033b9475a4990f2f8ac9
Muhammad Bilal Akbar <mbilalakbarkh786@gmail.com>
2023-04-19 17:28:59 +0500

    Final version

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_map_package/pages/home_page.dart';
+
+import 'my_app.dart';
 
 void main() => runApp(const MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        textTheme: const TextTheme(
-          bodyMedium: TextStyle(fontSize: 20),
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-              textStyle: const TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-              ),
-              padding:
-                  const EdgeInsets.symmetric(vertical: 12, horizontal: 24)),
-        ),
-        appBarTheme: const AppBarTheme(
-          centerTitle: true,
-          titleTextStyle: TextStyle(
-            fontSize: 30,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
-      home: const HomePage(),
-    );
-  }
-}
